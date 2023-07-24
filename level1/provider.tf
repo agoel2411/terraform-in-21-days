91758c336dc0d14e97708860678efcd6652eff14
agoel2411 <agoel2411@gmail.com>
2023-07-25 01:23:57 +0530

    Added layered approach in terraform code

---
+++ level1/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-remote-state-ashishgoel"
+    key            = "level1.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-remote-state"
+  }  
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ level2/terraform.tfvars
@@ -0,0 +1,3 @@
+env_code = "poc"
+
+vpc_cidr = "10.0.0.0/16"
