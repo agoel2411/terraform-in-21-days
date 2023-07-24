53e67e0c70cbdc799e2a4cff4daeda36d341ccd8
agoel2411 <138428992+agoel2411@users.noreply.github.com>
2023-07-25 01:47:07 +0530

    Added layered approach in terraform code (#10)
    * Added layered approach in terraform code
    
    * Added layered approach in terraform code
    
    * Added layered approach in terraform code

--- level2/instance.tf
+++ level2/instance.tf
@@ -10,6 +10,7 @@ data "aws_ami" "amazonlinux" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
   owners           = ["137112412989"]  
 }
 
@@ -19,7 +20,7 @@ resource "aws_instance" "public" {
   instance_type               = "t3.micro"
   key_name                    = "main"
   vpc_security_group_ids      = [aws_security_group.public.id]
-  subnet_id                   = aws_subnet.public[0].id
+  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[1]
 
   user_data = file("user-data.sh")
 
@@ -31,7 +32,7 @@ resource "aws_instance" "public" {
 resource "aws_security_group" "public" {
   name        = "${var.env_code}-public"
   description = "Allow inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
 
   ingress {
     description = "SSH from public"
@@ -66,7 +67,7 @@ resource "aws_instance" "private" {
   instance_type          = "t3.micro"
   key_name               = "main"
   vpc_security_group_ids = [aws_security_group.private.id]
-  subnet_id              = aws_subnet.private[0].id
+  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[1]
   tags = {
     Name = "${var.env_code}-private"
   }
@@ -75,14 +76,14 @@ resource "aws_instance" "private" {
 resource "aws_security_group" "private" {
   name        = "${var.env_code}-private"
   description = "Allow VPC traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
 
   ingress {
     description = "SSH from VPC"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
+    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
   }
 
   egress {
