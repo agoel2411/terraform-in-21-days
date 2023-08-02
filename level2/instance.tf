0c31cef91c80eded23c6f87159e54253e8b5c59e
agoel2411 <138428992+agoel2411@users.noreply.github.com>
2023-08-02 20:28:56 +0530

    Created Load balancer (#12)
    * Added load balancer configuration
    
    * Added load balancer configuration
    
    * Added load balancer configuration

--- level2/instance.tf
+++ level2/instance.tf
@@ -18,11 +18,9 @@ resource "aws_instance" "public" {
   ami                         = data.aws_ami.amazonlinux.id
   associate_public_ip_address = true
   instance_type               = "t3.micro"
-  key_name                    = "main"
+  key_name                    = "poc"
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[1]
-
-  user_data = file("user-data.sh")
 
   tags = {
     Name = "${var.env_code}-public"
@@ -39,7 +37,7 @@ resource "aws_security_group" "public" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["103.171.64.3/32"]
+    cidr_blocks = ["103.205.150.186/32"]
   }
   
   ingress {
@@ -47,7 +45,7 @@ resource "aws_security_group" "public" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["103.171.64.3/32"]
+    cidr_blocks = ["103.205.150.186/32"]
   }  
 
   egress {
@@ -65,9 +63,12 @@ resource "aws_security_group" "public" {
 resource "aws_instance" "private" {
   ami                    = data.aws_ami.amazonlinux.id
   instance_type          = "t3.micro"
-  key_name               = "main"
+  key_name               = "poc"
   vpc_security_group_ids = [aws_security_group.private.id]
   subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[1]
+
+  user_data = file("user-data.sh")
+
   tags = {
     Name = "${var.env_code}-private"
   }
@@ -84,6 +85,14 @@ resource "aws_security_group" "private" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
+  }
+
+  ingress{
+    description = "HTTP from Load Balancer"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups =[aws_security_group.load_balancer.id]
   }
 
   egress {
