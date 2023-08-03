bef20161660cd82746e760be17af450f9eb7b2d4
agoel2411 <agoel2411@gmail.com>
2023-08-03 20:12:45 +0530

    added autoscaling group

--- level2/instance.tf
+++ level2/instance.tf
@@ -17,7 +17,7 @@ data "aws_ami" "amazonlinux" {
 resource "aws_instance" "public" {
   ami                         = data.aws_ami.amazonlinux.id
   associate_public_ip_address = true
-  instance_type               = "t3.micro"
+  instance_type               = "t2.micro"
   key_name                    = "poc"
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[1]
@@ -62,7 +62,7 @@ resource "aws_security_group" "public" {
 
 resource "aws_instance" "private" {
   ami                    = data.aws_ami.amazonlinux.id
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   key_name               = "poc"
   vpc_security_group_ids = [aws_security_group.private.id]
   subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[1]
